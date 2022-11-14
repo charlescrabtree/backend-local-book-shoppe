433acb91a8cc194747b8f8e28e01492823b7ecda
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-11-14 15:04:39 -0800

    added test for author details

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -18,6 +18,23 @@ describe('author routes', () => {
     });
   });
 
+  it('GET /authors/:id should return author details and their books', async () => {
+    const res = await request(app).get('/authors/4');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      dob: expect.any(String),
+      pob: expect.any(String),
+      books: expect.any(Array),
+    });
+    expect(res.body.books[0]).toEqual({
+      id: expect.any(Number),
+      title: expect.any(String),
+      released: expect.any(Number),
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
