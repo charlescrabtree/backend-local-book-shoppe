99714f593298dadeb4727c0b52cdb2acc261fc2a
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-11-14 16:37:15 -0800

    added first test to books.test.js

---
+++ __tests__/books.test.js
@@ -0,0 +1,25 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('book routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('/books should return list of all books', async () => {
+    const res = await request(app).get('/books');
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(10);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      released: expect.any(Number),
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
